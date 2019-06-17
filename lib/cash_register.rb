d61f13dfebef9b09fc01f94b1a24f98b0b990ebf
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-17 12:05:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,5 +3,24 @@ class CashRegister
     @total = 0
   end
 
+  def total
+    puts
+  end
+
+  def add_item(title, price)
+
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+
+  end
+
+  def void_last_transaction
+
+  end
 
 end
